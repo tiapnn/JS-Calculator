32cfd8f365fcecb957c2a8d7f5c5617d8c191b7b
tiapnn <tiapnn@yahoo.it>
2020-05-05 11:56:59 +0200

    Made base and style

---
+++ main.js
@@ -0,0 +1,44 @@
+function displayNum(num) {
+  var currNum = $('#display').html();
+  currNum = currNum + num
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//for later use
+
+// var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+
+// var color = Math.floor(Math.random() * colors.length);
+  
+// $("html body").animate(
+//   {
+//     backgroundColor: colors[color],
+//     color: colors[color]
+//   },
+//   1000
+// );
+
+// $(".button").animate(
+//   {
+//     backgroundColor: colors[color]
+//   },
+//   1000
+// );
